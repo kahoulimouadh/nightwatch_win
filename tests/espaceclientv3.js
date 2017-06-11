5c2af688922c75f47b15abc554f589284f5400cf
mouadh kahouli <mouadh.kahouli.isi@gmail.com>
2017-06-11 18:58:57 +0100

    add browser drivers

--- tests/espaceclientv3.js
+++ tests/espaceclientv3.js
@@ -2,7 +2,6 @@ module.exports = {
   'espace client v3: step one' : function (browser) {
     browser
       .url('https://espaceclientv3.orange.fr')
-      .pause(1000)
       .waitForElementVisible('body', 1000)
       .assert.elementPresent("#o-nav-item-login")
       .click('a[title=Identifiez-vous]')
@@ -10,14 +9,9 @@ module.exports = {
 
   'espace client v3: step two' : function (browser) {
     browser
-      .url('https://id.orange.fr/auth_user/bin/auth_user.cgi?return_url=http://www.orange.fr/portail')
-      .pause(1000)
       .setValue('#default_f_credential', '0672088197')
-      .setValue('#default_f_password', '0672088197')
-      .click('input[type=submit]')
-      .pause(1000)
-      .url('https://espaceclientv3.orange.fr')
-      .pause(1000)
-      .end();
+      .setValue('#default_f_password', 'Test.123')
+      .click('input[value=s’identifier]')
+      .pause(5000)
   }
 };
