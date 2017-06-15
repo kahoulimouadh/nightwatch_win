627a534b88f7963e91109d2617e8ee69acb3d5e6
mouadh kahouli <mouadh.kahouli@sofrecom.com>
2017-06-15 10:12:55 +0100

    test page 1

--- pages/espaceClient.js
+++ pages/espaceClient.js
@@ -12,8 +12,20 @@ module.exports = {
       selector: '//input[@class="sc_button_content_2 submit"]',
       locateStrategy: 'xpath'
     },
-    bandeau: '.ec-account-gp',
-    avatar: '.ec-icon',
+    bandeau: '.ec-account-first',
+    avatar: '.ec-account-img',
+    iconAvatar: '.icon-avatar',
+    accountName: '.ec-account-name',
     infoPerso: '.btn-default-alone-sosh',
+    accountMail: {
+      selector: '//p[@class="ec-account-infos"][1]',
+      locateStrategy: 'xpath'
+    },
+    accountNum: {
+      selector: '//p[@class="ec-account-infos"][2]',
+      locateStrategy: 'xpath'
+    },
+    btn1: '.ec-account-btn .btn-default-alone-sosh',
+    btn2: '.ec-account-btn .btn-default'
   }
 };
--- tests/espaceclientv3.js
+++ tests/espaceclientv3.js
@@ -1,18 +1,48 @@
 module.exports = {
-  'espace client v3: step one' : function (browser) {
+  'espace client v3: page 1' : function (browser) {
     var page = browser.page.espaceClient();
     page.navigate()
       .waitForElementVisible('body', 1000)
       .assert.elementPresent("@loginItem")
       .click('@idButton')
 
-      .setValue('@authId', '0672088197')
-      .setValue('@authPassword', 'Test.123')
+      .setValue('@authId', 'giacomo.porcu@orange.fr')
+      .setValue('@authPassword', 'olmedo36')
       .click('@idSubmit')
 
       .waitForElementVisible("@bandeau", 5000)
       .assert.elementPresent("@avatar")
       .assert.elementPresent("@infoPerso")
+      .assert.elementPresent("@iconAvatar")
+      .assert.containsText("@accountName", "Votre compte Orange")
+      .assert.containsText("@accountMail", "mireille48@orange.fr")
+      .assert.containsText("@accountNum", "06 89 70 28 77")
+      .assert.attributeContains('@btn1', 'href', 'espaceclientv3.orange.fr')
+      .assert.attributeContains('@btn2', 'href', 'espaceclientv3.orange.fr')
+      .click('@btn2')
+      browser.pause(2000)
+    }/*,
+
+    'espace client v3: page 2' : function (browser) {
+    var page = browser.page.espaceClient();
+    page.navigate()
+      .waitForElementVisible('body', 1000)
+      .assert.elementPresent("@loginItem")
+      .click('@idButton')
+
+      .setValue('@authId', 'giacomo.porcu@orange.fr')
+      .setValue('@authPassword', 'olmedo36')
+      .click('@idSubmit')
+
+      .waitForElementVisible("@bandeau", 5000)
+      .assert.elementPresent("@avatar")
+      .assert.elementPresent("@infoPerso")
+      .assert.elementPresent("@iconAvatar")
+      .assert.containsText("@accountName", "Votre compte Orange")
+      .assert.containsText("@accountMail", "mireille48@orange.fr")
+      .assert.containsText("@accountNum", "06 89 70 28 77")
+      .assert.attributeContains('.btn-default-alone-sosh', 'href', 'espaceclientv3.orange.fr')
+      .assert.attributeContains('.btn-default', 'href', 'espaceclientv3.orange.fr')
       browser.end();
-    }
+    }*/
 };
